75ebdd0eb31193e95fcd12d6186a2c155002966e
aramisramirez <brunomelendez110@gmail.com>
2025-06-16 10:13:38 -0600

    se añadio el script de la bd

--- obj/Debug/net9.0/login.AssemblyInfo.cs
+++ obj/Debug/net9.0/login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e079e5f7fce19d027c4e07f9c559138ffa40255a")]
 [assembly: System.Reflection.AssemblyProductAttribute("login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
